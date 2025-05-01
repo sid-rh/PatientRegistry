c4aa948bca03a1fcf840062e650578c2df6a320f
sid-rh <siddhanth.rh@gmail.com>
2025-05-01 14:31:44 +0530

    Display records on home page added

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import { useLiveQuery } from '@electric-sql/pglite-react';
+import React,{useState} from 'react';
+import { useLiveQuery,usePGlite } from '@electric-sql/pglite-react';
 
 const PatientList = () => {
 
-    const rows=useLiveQuery(
-        'SELECT * FROM patients ORDER BY created_at DESC;'
+    // const [rows,setRows]=useState([]);
+
+    // const db = usePGlite();
+    // db.live.query(
+    //     'SELECT * FROM patients ORDER BY created_at DESC;',
+    //     [],
+    //     (res)=>{
+    //         setRows(res);
+    //     }
+    // );
+
+    const res=useLiveQuery(
+        'SELECT * FROM patients ORDER BY created_at DESC;',
     );
-    console.log(rows);
-    
+    const data=res!==undefined?res.rows:[];
   return (
     <div>
-        {
-        
+        { data&&
+        (data.map((patient) => (
+            <li key={patient.id}>
+              <strong>{patient.name}</strong> &ndash; {patient.dob.toLocaleDateString()} &ndash; {patient.email}
+            </li>
+          )))
       }
     </div>
   )
